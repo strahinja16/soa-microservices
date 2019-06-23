1e58407f9c124487a89446cdbfdb73dd30116562
JKostov <julijekos@gmail.com>
2019-06-23 11:04:34 +0200

    Exception log

--- StatisticMicroservice/Services/MqttService.cs
+++ StatisticMicroservice/Services/MqttService.cs
@@ -2,6 +2,7 @@
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
+using System;
 
 namespace StatisticMicroservice.Services
 {
@@ -12,20 +13,36 @@ namespace StatisticMicroservice.Services
         {
             var factory = new MqttFactory();
             Client = factory.CreateMqttClient();
-            var options = new MqttClientOptionsBuilder().WithClientId("Statistics-microservice").WithTcpServer(configuration.GetSection("MqttUrl").Value).Build();
-            Client.ConnectAsync(options);
+            var options = new MqttClientOptionsBuilder().WithClientId("Statistics-microservice").WithTcpServer(configuration.GetSection("MqttUrl").Value, 1883).Build();
+            try 
+            {
+                Client.ConnectAsync(options);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("MQTT CONNECT EXCEPTION");
+                Console.WriteLine(e.Message);
+            }
         }
 
         async public void PublishMessage(string topic, string data)
         {
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic(topic)
-                .WithPayload(data)
-                //.WithExactlyOnceQoS()
-                //.WithRetainFlag()
-                .Build();
+            if (!Client.IsConnected)
+            {
+                Console.WriteLine("CLIENT NOT CONNECTED WHILE SENDING MESSAGE");
+            }
+            else
+            {
+                Console.WriteLine("PUBLISH MESSAGE");
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(data)
+                    //.WithExactlyOnceQoS()
+                    //.WithRetainFlag()
+                    .Build();
 
-            await Client.PublishAsync(message);
+                await Client.PublishAsync(message);
+            }
         }
     }
 }
